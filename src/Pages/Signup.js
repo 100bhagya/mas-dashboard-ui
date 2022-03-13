f62c452cb43e59b1f172719b3e8b1f8f2cccd1ab
PAWAN KUMAR SAHU <89339624+coderpawan@users.noreply.github.com>
2022-03-13 23:10:08 +0530

    Signup (#7)
    * calender added
    
    * commit
    
    * commit

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,13 +1,16 @@
 import React from "react";
 import {
-  FaFacebookF,
-  FaGoogle,
-  FaLinkedinIn,
+  // FaFacebookF,
+  // FaGoogle,
+  // FaLinkedinIn,
+
   FaRegEnvelope,
 } from "react-icons/fa";
 import { MdLockOutline } from "react-icons/md";
 import { Link } from "react-router-dom";
 import { AiOutlineUser } from "react-icons/ai";
+import Logo from "../images/logo.png";
+
 
 const Signup = () => {
   return (
@@ -16,14 +19,15 @@ const Signup = () => {
         <div className="bg-white rounded-2xl shadow-2xl flex w-2/3 max-w-4xl">
           <div className="w-3/5 p-5">
             <div className="text-left font-bold">
-              <span className="text-green-500">MyAnalytics</span>School
+              <img src={Logo} alt="logo" className="h-10" />
             </div>
             <div className="py-10">
-              <h2 className="text-3xl font-bold text-green-500 mb-2">
+              <h2 className="text-3xl font-bold text-blue-600 mb-2">
                 Create your Account
               </h2>
-              <div className="bg-green-500 h-1 w-10 inline-block mb-2"></div>
-              <div className="flex justify-center my-2">
+              <div className="bg-blue-600 h-1 w-10 inline-block mb-2"></div>
+              {/* <div className="flex justify-center my-2">
+
                 <a
                   href="/"
                   className="border-2 border-gray-200 rounded-full p-3 mx-1"
@@ -43,7 +47,8 @@ const Signup = () => {
                   <FaGoogle className="text-sm" />
                 </a>
               </div>
-              <p className="text-gray-400 my-3">or use your email account</p>
+              <p className="text-gray-400 my-3">or use your email account</p> */}
+
               <div className="flex flex-col items-center">
                 <div className="bg-gray-100 w-64 p-2 flex items-center mb-3">
                   <AiOutlineUser className="text-gray-400 m-2" />
@@ -72,7 +77,8 @@ const Signup = () => {
                     className="bg-gray-100 outline-none text-sm flex-1"
                   />
                 </div>
-                <div className="bg-gray-100 w-64 p-2 flex items-center">
+                <div className="bg-gray-100 w-64 p-2 flex items-center mb-3">
+
                   <MdLockOutline className="text-gray-400 m-2" />
                   <input
                     type="password"
@@ -81,31 +87,34 @@ const Signup = () => {
                     className="bg-gray-100 outline-none text-sm flex-1"
                   />
                 </div>
-                <div className="flex justify-between w-64 mb-5 mt-2">
-                  <label className="flex items-center text-xs">
-                    <input
-                      type="checkbox"
-                      name="remember"
-                      className="mr-1"
-                    ></input>
-                    Remember me
-                  </label>
+                <div className="bg-gray-100 w-64 p-2 flex items-center">
+                  <MdLockOutline className="text-gray-400 m-2" />
+                  <input
+                    type="password"
+                    name="confirmpassword"
+                    placeholder="Confirm Password"
+                    className="bg-gray-100 outline-none text-sm flex-1"
+                  />
                 </div>
+
                 <a
                   href="/"
-                  className="border-2 border-green-500 text-green-500 rounded-full px-12 py-2 inline-block font-semibold hover:bg-green-500 hover:text-white"
+                  className="border-2 mt-10 border-blue-600 text-blue-600 rounded-full px-12 py-2 inline-block font-semibold hover:bg-blue-600 hover:text-white"
+
                 >
                   Sign Up
                 </a>
               </div>
             </div>
           </div>
-          <div className="w-2/5 bg-green-500 text-white rounded-tr-2xl rounded-br-2xl py-36 px-12">
+          <div className="w-2/5 bg-blue-600 text-white rounded-tr-2xl rounded-br-2xl py-36 px-12">
+
             <h2 className="text-3xl  font-bold mb-2">Hello, Friend!</h2>
             <div className="bg-white h-1 w-10 inline-block mb-2"></div>
             <p className="mb-10">Already have an account then Sign In</p>
             <Link to="/signin">
-              <div className="border-2 border-white rounded-full px-12 py-2 inline-block font-semibold hover:bg-white hover:text-green-500">
+              <div className="border-2 border-white rounded-full px-12 py-2 inline-block font-semibold hover:bg-white hover:text-blue-600">
+
                 Sign In
               </div>
             </Link>
