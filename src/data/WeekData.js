4d572e7352f30fac571e22b064e803e7a7e5ce7a
PAWAN KUMAR SAHU <89339624+coderpawan@users.noreply.github.com>
2022-03-18 00:18:09 +0530

    Weeks Panel Added

---
+++ src/data/WeekData.js
@@ -0,0 +1,424 @@
+const WeekData = [
+  {
+    id: 1,
+    question: "Week 1",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: true,
+  },
+  {
+    id: 2,
+    question: "Week 2",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 3,
+    question: "Week 3",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 4,
+    question: "Week 4",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 5,
+    question: "Week 5",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 6,
+    question: "Week 6",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 7,
+    question: "Week 7",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 8,
+    question: "Week 8",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 9,
+    question: "Week 9",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 10,
+    question: "Week 10",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 11,
+    question: "Week 11",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 12,
+    question: "Week 12",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 13,
+    question: "Week 13",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 14,
+    question: "Week 14",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 15,
+    question: "Week 15",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 16,
+    question: "Week 16",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 17,
+    question: "Week 17",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 18,
+    question: "Week 18",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 19,
+    question: "Week 19",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 20,
+    question: "Week 20",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 21,
+    question: "Week 21",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 22,
+    question: "Week 22",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 23,
+    question: "Week 23",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 24,
+    question: "Week 24",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 25,
+    question: "Week 25",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 26,
+    question: "Week 26",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 27,
+    question: "Week 27",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 28,
+    question: "Week 28",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 29,
+    question: "Week 29",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+  {
+    id: 30,
+    question: "Week 30",
+
+    answer: [
+      {
+        role: "Article 1",
+      },
+      {
+        role: "Article 2",
+      },
+    ],
+    open: false,
+  },
+];
+
+export default WeekData;
