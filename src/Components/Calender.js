092dd3d12d84286b929c2bba6878fa2ae9acdc37
ajaysharmadev <ajaysharma.13122000@gmail.com>
2022-10-17 12:50:49 +0530

    Notcompleted/Completed Functionality added to calendar

--- src/Components/Calender.js
+++ src/Components/Calender.js
@@ -5,6 +5,12 @@ import moment from "moment";
 
 export default function App(props) {
   const [dateState, setDateState] = useState(new Date());
+  const mark = ["12-10-2022", "13-10-2022", "14-10-2022"];
+  const markTwo = {
+    "12-10-2022":"completed",
+    "13-10-2022":"not-completed",
+    "14-10-2022":"completed"
+  };
 
   const Dated = moment(dateState).format("DD-MM-YYYY");
   return (
@@ -13,6 +19,14 @@ export default function App(props) {
         value={dateState}
         onChange={(e) => {
           setDateState(e);
+        }}
+        tileClassName={({ date, view }) => {
+          if (markTwo[moment(date).format("DD-MM-YYYY")] === "completed") {
+            return "completed-words";
+          }
+          if (markTwo[moment(date).format("DD-MM-YYYY")] === "not-completed") {
+            return "not-completed-words";
+          }
         }}
         className="rounded-2xl shadow-2xl shadow-blue-100"
       />
