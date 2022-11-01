31cd09507cbf47cd24f8765c6f6b253d0c2e5021
Ajay Sharma <hi@ajaysharma.dev>
2022-11-01 20:15:41 +0530

    Added Functionality: Update Profile

--- src/Components/Account.jsx
+++ src/Components/Account.jsx
@@ -1,6 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRef } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
+import { API_BASE_URL } from "../data/consts";
+import axios from "axios";
+import LoadingSpinner from "./LoadingSpinner";
 
 const Account = () => {
+  const { loginInfo } = useContext(AuthContext);
+  var token = loginInfo.accessToken;
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
+  const emailRef = useRef();
+  const postalCodeRef = useRef();
+  const stateRef = useRef();
+  const cityRef = useRef();
+  const addressRef = useRef();
+  const userNameRef = useRef();
+  const phoneNumberRef = useRef();
+  const [isLoading, setIsLoading] = useState(false);
+  const handleFetchProfile = () => {};
+  const handleUpdateProfile = () => {
+    let bodyParameters = {
+      firstName: firstNameRef.current.value.trim(),
+      lastName: lastNameRef.current.value.trim(),
+      username: userNameRef.current.value.trim(),
+      // profilePic: "No IMG URL provided",
+      phoneNo: parseInt(phoneNumberRef.current.value.trim()), //TODO
+      address: addressRef.current.value.trim(),
+      postalCode: postalCodeRef.current.value.trim(),
+      state: stateRef.current.value.trim(),
+      city: cityRef.current.value.trim(),
+    };
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    if (
+      !(
+        firstNameRef.current.value === "" ||
+        lastNameRef.current.value === "" ||
+        postalCodeRef.current.value === "" ||
+        stateRef.current.value === "" ||
+        cityRef.current.value === "" ||
+        addressRef.current.value === "" ||
+        userNameRef.current.value === "" ||
+        phoneNumberRef.current.value === ""
+      )
+    ) {
+      setIsLoading(true);
+      axios
+        .put(`${API_BASE_URL}/api/updateProfile`, bodyParameters, config)
+        .then((response) => {
+          console.log(bodyParameters);
+          console.log(response.data);
+          firstNameRef.current.value = response.data.firstName;
+          lastNameRef.current.value = response.data.lastName;
+          userNameRef.current.value = response.data.username;
+          phoneNumberRef.current.value = response.data.phoneNo;
+          addressRef.current.value = response.data.address;
+          cityRef.current.value = response.data.city;
+          stateRef.current.value = response.data.state;
+          postalCodeRef.current.value = response.data.postalCode;
+          emailRef.current.value = response.data.email;
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+          setIsLoading(false);
+        });
+    }
+  };
   return (
     <div className="flex justify-center py-12">
       <div className="flex flex-col gap-6">
@@ -19,6 +88,7 @@ const Account = () => {
                 First Name
               </div>
               <input
+                ref={firstNameRef}
                 type="text"
                 className="min-w-[250px] p-1 rounded-md border border-gray-300"
               />
@@ -28,6 +98,7 @@ const Account = () => {
                 Last Name
               </div>
               <input
+                ref={lastNameRef}
                 type="text"
                 className="min-w-[250px] p-1 rounded-md border border-gray-300"
               />
@@ -38,6 +109,7 @@ const Account = () => {
           <div className="flex flex-col gap-1">
             <div className="text-sm text-slate-600 font-medium">Username</div>
             <input
+              ref={userNameRef}
               type="text"
               className="max-w-[350px] p-1 rounded-md border border-gray-300"
             />
@@ -47,6 +119,7 @@ const Account = () => {
           <div className="font-medium">Photo</div>
           <div className="flex gap-6 items-center">
             <img
+              alt="User Profile"
               className="w-10 h-10 rounded-full"
               src="https://media-exp1.licdn.com/dms/image/C4D03AQHj-Uul_quJmA/profile-displayphoto-shrink_400_400/0/1663409602983?e=1672272000&v=beta&t=-3uvoSvXfJpSjjZ5ruXq3JsLt8eM82ciNYOYoMWkLc4"
             />
@@ -73,8 +146,10 @@ const Account = () => {
             <div className="flex flex-col gap-1">
               <div className="text-sm text-slate-600 font-medium">Email</div>
               <input
+                ref={emailRef}
+                disabled
                 type="text"
-                className="min-w-[250px] p-1 rounded-md border border-gray-300"
+                className="min-w-[250px] p-1 rounded-md border border-gray-300 cursor-not-allowed"
               />
             </div>
             <div className="flex flex-col gap-1">
@@ -82,6 +157,7 @@ const Account = () => {
                 Phone Number
               </div>
               <input
+                ref={phoneNumberRef}
                 type="text"
                 className="min-w-[250px] p-1 rounded-md border border-gray-300"
               />
@@ -97,6 +173,7 @@ const Account = () => {
                 Postal Code
               </div>
               <input
+                ref={postalCodeRef}
                 type="text"
                 className="min-w-[100px] p-1 rounded-md border border-gray-300"
               />
@@ -104,6 +181,7 @@ const Account = () => {
             <div className="flex flex-col gap-1">
               <div className="text-sm text-slate-600 font-medium">State</div>
               <input
+                ref={stateRef}
                 type="text"
                 className="min-w-[100px] p-1 rounded-md border border-gray-300"
               />
@@ -111,6 +189,7 @@ const Account = () => {
             <div className="flex flex-col gap-1">
               <div className="text-sm text-slate-600 font-medium">City</div>
               <input
+                ref={cityRef}
                 type="text"
                 className="min-w-[100px] p-1 rounded-md border border-gray-300"
               />
@@ -121,6 +200,7 @@ const Account = () => {
         <div className="flex flex-col gap-1">
           <div className="text-sm text-slate-600 font-medium">Address</div>
           <input
+            ref={addressRef}
             type="text"
             className="w-full p-1 rounded-md border border-gray-300"
           />
@@ -129,12 +209,24 @@ const Account = () => {
         <hr class="my-8 h-px bg-gray-200 border-0 dark:bg-gray-200"></hr>
 
         <div className="flex justify-end gap-6">
-          <button className="py-1 px-2 rounded-md border border-gray-300 bg-white">
+          <button
+            onClick={handleFetchProfile}
+            className="py-1 px-2 rounded-md border border-gray-300 bg-white"
+          >
             Cancel
           </button>
-          <button className="py-1 px-2 rounded-md border border-blue-500 bg-blue-500 text-white">
-            Save
-          </button>
+          {isLoading ? (
+            <div className="px-2">
+              <LoadingSpinner />
+            </div>
+          ) : (
+            <button
+              onClick={handleUpdateProfile}
+              className="py-1 px-2 rounded-md border border-gray-300 bg-blue-500 text-white"
+            >
+              Update
+            </button>
+          )}
         </div>
       </div>
     </div>
