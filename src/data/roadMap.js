ab83a607e0c1f823e102bd6e580d45129a1ab7f2
Harry <harikeshkrsingh20@gmail.com>
2023-04-16 22:31:26 +0530

    added some more data in roadmap

--- src/data/roadMap.js
+++ src/data/roadMap.js
@@ -1,7 +1,7 @@
 const Data = [
     {
       id: 1,
-      question: "Week 1",
+      question: "Week 1  (3 Apr - 9 Apr)",
       Status: "MAS101 Onboarding",
       
       answer: [
@@ -64,6 +64,832 @@ const Data = [
       ],
       open: false,
     },
+    {
+      id: 4,
+      question: "Week 4 ",
+      answer: [
+        {
+          role: "Self Evaluation Program",
+          date: "24 Apr - 30 Apr",
+        },
+        {
+          role: "SQL Course",
+          date: "24 Apr - 30 Apr",
+        },
+        {
+          role: "WT 2",
+          date: "30 Apr",
+        },
+       
+      ],
+      open: false,
+    },
+    {
+      id: 5,
+      question: "Week 5",
+      answer: [
+        {
+          role: "Self Evaluation Program",
+          date: "1 May - 7 May",
+        },
+        {
+          role: "SQL Course",
+          date: "1 May - 7 May",
+        },
+        {
+          role: "VARC 2",
+          date: "7 May",
+        },
+       
+      ],
+      open: false,
+    },
+    {
+      id: 6,
+      question: "Week 6",
+      answer: [
+        {
+          role: "Self Evaluation Program",
+          date: "8 May - 14 May",
+        },
+        // {
+        //   role: "SQL Course",
+        //   date: "17 Apr - 23 Apr",
+        // },
+        {
+          role: "WT 3",
+          date: "14 May",
+        },
+       
+      ],
+      open: false,
+    },
+    {
+      id: 7,
+      question: "Week 7 ",
+      answer: [
+        {
+          role: "Percentages, Profit & Loss",
+          date: "15 May - 19 May",
+        },
+        {
+          role: "Excel + Stats Course ",
+          date: "15 May - 19 May",
+        },
+        {
+          role: "PPL Sectional Test ",
+          date: "20 May",
+        },
+        {
+          role: "Simple & Compound Interest",
+          date: "21 May",
+        },
+        {
+          role: "VARC 3",
+          date: "21 May",
+        },
+       
+      ],
+      open: false,
+    },
+    {
+      id: 8,
+      question: "Week 8 ",
+      answer: [
+        {
+          role: "Excel + Stats Course ",
+          date: "22 May - 28 May",
+        },
+        {
+          role: "Simple & Compound Interest",
+          date: "22 May",
+        },
+        {
+          role: "SI&CI Sectional Test ",
+          date: "23 May",
+        },
+        {
+          role: "Time & Work ",
+          date: "24 May - 28 May",
+        },
+        {
+          role: "WT 4",
+          date: "24 May - 28 May",
+        },
+       
+      ],
+      open: false,
+    },
+    {
+      id: 9,
+      question: "Week 9 ",
+      answer: [
+        {
+          role: "Time & Work Sectional Test ",
+          date: "29 May",
+        },
+        {
+          role: "Average, Mixture & Allegations ",
+          date: "30 Jun - 1 Jun ",
+        },
+        {
+          role: "AMA Sectional Test ",
+          date: "2 Jun",
+        },
+        {
+          role: "Time, Speed & Distance ",
+          date: "3 Jun - 4 Jun",
+        },
+        {
+          role: "VARC 4 ",
+          date: "4 Jun",
+        },
+       
+      ],
+      open: false,
+    },
+    {
+      id: 10,
+      question: "Week 10 ",
+      answer: [
+        {
+          role: "Time, Speed & Distance ",
+          date: "5 Jun - 6 Jun",
+        },
+        {
+          role: "Python Course",
+          date: "5 Jun - 11 Jun",
+        },
+        {
+          role: "TSD Sectional Test ",
+          date: "7 Jun",
+        },
+        {
+          role: "P & C ",
+          date: "8 Jun - 11 Jun",
+        },
+        {
+          role: "WT 5",
+          date: "11 Jun",
+        },
+       
+      ],
+      open: false,
+    },
+    
+    {
+      id: 11,
+      question: "Week 11 ",
+      answer: [
+        {
+          role: "P&C Sectional Test ",
+          date: "12 Jun",
+        },
+        {
+          role: "ML Course + Coding",
+          date: "12 Jun - 18 Jun",
+        },
+        {
+          role: "Proabability",
+          date: "13 Jun - 16 Jun",
+        },
+        {
+          role: "Probability Sectional Test ",
+          date: "17 Jun",
+        },
+        {
+          role: "VARC 5",
+          date: "18 Jun",
+        },
+       
+      ],
+      open: false,
+    },
+    {
+      id: 12,
+      question: "Week 12 ",
+      answer: [
+        {
+          role: "Self Evaluation Program",
+          date: "19 Jun - 25 Jun",
+        },
+        {
+          role: "Linear & Circular Arrangement",
+          date: "22 Jun - 25 Jun",
+        },
+        {
+          role: "WT 6",
+          date: "25 Jun",
+        },
+       
+      ],
+      open: false,
+    },
+    {
+      id: 13,
+      question: "Week 13 ",
+      answer: [
+        {
+          role: "Linear Arrangement Sectional Test ",
+          date: "26 Jun",
+        },
+        {
+          role: "ML Course",
+          date: "26 Jun - 2 July",
+        },
+        {
+          role: "Circular Arrangement Sectional Test ",
+          date: "27 Jun",
+        },
+        {
+          role: "Line, Bar, Column, Pie Charts, Tables ",
+          date: "28 Jun - 2 Jul",
+        },
+        {
+          role: "VARC 6",
+          date: "2 Jul",
+        },
+       
+      ],
+      open: false,
+    },
+    {
+      id: 14,
+      question: "Week 14 ",
+      answer: [
+        {
+          role: "Charts Sectional Test ",
+          date: "3 Jul",
+        },
+        {
+          role: "ML Course ",
+          date: "3 Jul - 9 Jul",
+        },
+        {
+          role: "Tables Sectional Test ",
+          date: "4 Jul",
+        },
+        {
+          role: "Syllogism",
+          date: "5 Jul - 6 Jul",
+        },
+        {
+          role: "Syllogism Sectional Test ",
+          date: "7 Jul",
+        },
+        {
+          role: "Calendars & Clocks ",
+          date: "8 Jul - 9 Jul",
+        },
+        {
+          role: "WT 7 ",
+          date: "9 Jul",
+        },
+       
+      ],
+      open: false,
+    },
+    {
+      id: 15,
+      question: "Week 15 ",
+      answer: [
+        {
+          role: "Linear Arrangement Sectional Test ",
+          date: "26 Jun",
+        },
+        {
+          role: "ML Course",
+          date: "26 Jun - 2 July",
+        },
+        {
+          role: "Circular Arrangement Sectional Test ",
+          date: "27 Jun",
+        },
+        {
+          role: "Line, Bar, Column, Pie Charts, Tables ",
+          date: "28 Jun - 2 Jul",
+        },
+        {
+          role: "VARC 6",
+          date: "2 Jul",
+        },
+       
+      ],
+      open: false,
+    },
+    {
+      id: 16,
+      question: "Week 16 ",
+      answer: [
+        {
+          role: "Non Verbal Reasoning Sectional Test  ",
+          date: "17 Jul",
+        },
+        {
+          role: "Blood Relations",
+          date: "18 Jul",
+        },
+        {
+          role: "Blood Relations Sectional Test  ",
+          date: "19 Jul",
+        },
+        {
+          role: "Venn Diagram  ",
+          date: "20 Jul - 23 Jul",
+        },
+        {
+          role: "WT 8",
+          date: "23 Jul",
+        },
+        
+       
+      ],
+      open: false,
+    },
+    {
+      id: 17,
+      question: "Week 17 ",
+      answer: [
+        {
+          role: "Venn Diagram Sectional Test  ",
+          date: "24 Jul",
+        },
+        {
+          role: "Numbers",
+          date: "28 Jul - 30 Jul",
+        },
+        {
+          role: "VARC 8",
+          date: "30 Jul",
+        },
+       
+      ],
+      open: false,
+    },
+    {
+      id: 18,
+      question: "Week 18 ",
+      answer: [
+        {
+          role: "Numbers ",
+          date: "31 Jul",
+        },
+        {
+          role: "Project 1",
+          date: "31 Jul - 6 Aug",
+        },
+        {
+          role: "Product Course",
+          date: "31 Jul - 6 Aug",
+        },
+        {
+          role: "Resume Building (Self) ",
+          date: "31 Jul - 6 Aug",
+        },
+        {
+          role: "Numbers Sectional Test  ",
+          date: "1 Aug",
+        },
+        {
+          role: "Equations",
+          date: "2 Aug - 4 Aug",
+        },
+        {
+          role: "Equations Sectional Test ",
+          date: "5 Aug",
+        },
+        {
+          role: "Sequence & Series ",
+          date: "6 Aug",
+        },
+        {
+          role: "WT 9 ",
+          date: "6 Aug",
+        },
+       
+      ],
+      open: false,
+    },
+    // {
+    //   id: 19,
+    //   question: "Week 19 ",
+    //   answer: [
+    //     {
+    //       role: "Linear Arrangement Sectional Test ",
+    //       date: "26 Jun",
+    //     },
+    //     {
+    //       role: "ML Course",
+    //       date: "26 Jun - 2 July",
+    //     },
+    //     {
+    //       role: "Circular Arrangement Sectional Test ",
+    //       date: "27 Jun",
+    //     },
+    //     {
+    //       role: "Line, Bar, Column, Pie Charts, Tables ",
+    //       date: "28 Jun - 2 Jul",
+    //     },
+    //     {
+    //       role: "VARC 6",
+    //       date: "2 Jul",
+    //     },
+       
+    //   ],
+    //   open: false,
+    // },
+    // {
+    //   id: 20,
+    //   question: "Week 20",
+    //   answer: [
+    //     {
+    //       role: "Linear Arrangement Sectional Test ",
+    //       date: "26 Jun",
+    //     },
+    //     {
+    //       role: "ML Course",
+    //       date: "26 Jun - 2 July",
+    //     },
+    //     {
+    //       role: "Circular Arrangement Sectional Test ",
+    //       date: "27 Jun",
+    //     },
+    //     {
+    //       role: "Line, Bar, Column, Pie Charts, Tables ",
+    //       date: "28 Jun - 2 Jul",
+    //     },
+    //     {
+    //       role: "VARC 6",
+    //       date: "2 Jul",
+    //     },
+       
+    //   ],
+    //   open: false,
+    // },
+    // {
+    //   id: 21,
+    //   question: "Week 21 ",
+    //   answer: [
+    //     {
+    //       role: "Linear Arrangement Sectional Test ",
+    //       date: "26 Jun",
+    //     },
+    //     {
+    //       role: "ML Course",
+    //       date: "26 Jun - 2 July",
+    //     },
+    //     {
+    //       role: "Circular Arrangement Sectional Test ",
+    //       date: "27 Jun",
+    //     },
+    //     {
+    //       role: "Line, Bar, Column, Pie Charts, Tables ",
+    //       date: "28 Jun - 2 Jul",
+    //     },
+    //     {
+    //       role: "VARC 6",
+    //       date: "2 Jul",
+    //     },
+       
+    //   ],
+    //   open: false,
+    // },
+    // {
+    //   id: 22,
+    //   question: "Week 22 ",
+    //   answer: [
+    //     {
+    //       role: "Linear Arrangement Sectional Test ",
+    //       date: "26 Jun",
+    //     },
+    //     {
+    //       role: "ML Course",
+    //       date: "26 Jun - 2 July",
+    //     },
+    //     {
+    //       role: "Circular Arrangement Sectional Test ",
+    //       date: "27 Jun",
+    //     },
+    //     {
+    //       role: "Line, Bar, Column, Pie Charts, Tables ",
+    //       date: "28 Jun - 2 Jul",
+    //     },
+    //     {
+    //       role: "VARC 6",
+    //       date: "2 Jul",
+    //     },
+       
+    //   ],
+    //   open: false,
+    // },
+    // {
+    //   id: 23,
+    //   question: "Week 23 ",
+    //   answer: [
+    //     {
+    //       role: "Linear Arrangement Sectional Test ",
+    //       date: "26 Jun",
+    //     },
+    //     {
+    //       role: "ML Course",
+    //       date: "26 Jun - 2 July",
+    //     },
+    //     {
+    //       role: "Circular Arrangement Sectional Test ",
+    //       date: "27 Jun",
+    //     },
+    //     {
+    //       role: "Line, Bar, Column, Pie Charts, Tables ",
+    //       date: "28 Jun - 2 Jul",
+    //     },
+    //     {
+    //       role: "VARC 6",
+    //       date: "2 Jul",
+    //     },
+       
+    //   ],
+    //   open: false,
+    // },
+    // {
+    //   id: 24,
+    //   question: "Week 24 ",
+    //   answer: [
+    //     {
+    //       role: "Linear Arrangement Sectional Test ",
+    //       date: "26 Jun",
+    //     },
+    //     {
+    //       role: "ML Course",
+    //       date: "26 Jun - 2 July",
+    //     },
+    //     {
+    //       role: "Circular Arrangement Sectional Test ",
+    //       date: "27 Jun",
+    //     },
+    //     {
+    //       role: "Line, Bar, Column, Pie Charts, Tables ",
+    //       date: "28 Jun - 2 Jul",
+    //     },
+    //     {
+    //       role: "VARC 6",
+    //       date: "2 Jul",
+    //     },
+       
+    //   ],
+    //   open: false,
+    // },
+    // {
+    //   id: 25,
+    //   question: "Week 25 ",
+    //   answer: [
+    //     {
+    //       role: "Linear Arrangement Sectional Test ",
+    //       date: "26 Jun",
+    //     },
+    //     {
+    //       role: "ML Course",
+    //       date: "26 Jun - 2 July",
+    //     },
+    //     {
+    //       role: "Circular Arrangement Sectional Test ",
+    //       date: "27 Jun",
+    //     },
+    //     {
+    //       role: "Line, Bar, Column, Pie Charts, Tables ",
+    //       date: "28 Jun - 2 Jul",
+    //     },
+    //     {
+    //       role: "VARC 6",
+    //       date: "2 Jul",
+    //     },
+       
+    //   ],
+    //   open: false,
+    // },
+    // {
+    //   id: 26,
+    //   question: "Week 26 ",
+    //   answer: [
+    //     {
+    //       role: "Linear Arrangement Sectional Test ",
+    //       date: "26 Jun",
+    //     },
+    //     {
+    //       role: "ML Course",
+    //       date: "26 Jun - 2 July",
+    //     },
+    //     {
+    //       role: "Circular Arrangement Sectional Test ",
+    //       date: "27 Jun",
+    //     },
+    //     {
+    //       role: "Line, Bar, Column, Pie Charts, Tables ",
+    //       date: "28 Jun - 2 Jul",
+    //     },
+    //     {
+    //       role: "VARC 6",
+    //       date: "2 Jul",
+    //     },
+       
+    //   ],
+    //   open: false,
+    // },
+    // {
+    //   id: 27,
+    //   question: "Week 27 ",
+    //   answer: [
+    //     {
+    //       role: "Linear Arrangement Sectional Test ",
+    //       date: "26 Jun",
+    //     },
+    //     {
+    //       role: "ML Course",
+    //       date: "26 Jun - 2 July",
+    //     },
+    //     {
+    //       role: "Circular Arrangement Sectional Test ",
+    //       date: "27 Jun",
+    //     },
+    //     {
+    //       role: "Line, Bar, Column, Pie Charts, Tables ",
+    //       date: "28 Jun - 2 Jul",
+    //     },
+    //     {
+    //       role: "VARC 6",
+    //       date: "2 Jul",
+    //     },
+       
+    //   ],
+    //   open: false,
+    // },
+    // {
+    //   id: 28,
+    //   question: "Week 28 ",
+    //   answer: [
+    //     {
+    //       role: "Linear Arrangement Sectional Test ",
+    //       date: "26 Jun",
+    //     },
+    //     {
+    //       role: "ML Course",
+    //       date: "26 Jun - 2 July",
+    //     },
+    //     {
+    //       role: "Circular Arrangement Sectional Test ",
+    //       date: "27 Jun",
+    //     },
+    //     {
+    //       role: "Line, Bar, Column, Pie Charts, Tables ",
+    //       date: "28 Jun - 2 Jul",
+    //     },
+    //     {
+    //       role: "VARC 6",
+    //       date: "2 Jul",
+    //     },
+       
+    //   ],
+    //   open: false,
+    // },
+    // {
+    //   id: 29,
+    //   question: "Week 30 ",
+    //   answer: [
+    //     {
+    //       role: "Linear Arrangement Sectional Test ",
+    //       date: "26 Jun",
+    //     },
+    //     {
+    //       role: "ML Course",
+    //       date: "26 Jun - 2 July",
+    //     },
+    //     {
+    //       role: "Circular Arrangement Sectional Test ",
+    //       date: "27 Jun",
+    //     },
+    //     {
+    //       role: "Line, Bar, Column, Pie Charts, Tables ",
+    //       date: "28 Jun - 2 Jul",
+    //     },
+    //     {
+    //       role: "VARC 6",
+    //       date: "2 Jul",
+    //     },
+       
+    //   ],
+    //   open: false,
+    // },
+    // {
+    //   id: 31,
+    //   question: "Week 31 ",
+    //   answer: [
+    //     {
+    //       role: "Linear Arrangement Sectional Test ",
+    //       date: "26 Jun",
+    //     },
+    //     {
+    //       role: "ML Course",
+    //       date: "26 Jun - 2 July",
+    //     },
+    //     {
+    //       role: "Circular Arrangement Sectional Test ",
+    //       date: "27 Jun",
+    //     },
+    //     {
+    //       role: "Line, Bar, Column, Pie Charts, Tables ",
+    //       date: "28 Jun - 2 Jul",
+    //     },
+    //     {
+    //       role: "VARC 6",
+    //       date: "2 Jul",
+    //     },
+       
+    //   ],
+    //   open: false,
+    // },
+    // {
+    //   id: 32,
+    //   question: "Week 32 ",
+    //   answer: [
+    //     {
+    //       role: "Linear Arrangement Sectional Test ",
+    //       date: "26 Jun",
+    //     },
+    //     {
+    //       role: "ML Course",
+    //       date: "26 Jun - 2 July",
+    //     },
+    //     {
+    //       role: "Circular Arrangement Sectional Test ",
+    //       date: "27 Jun",
+    //     },
+    //     {
+    //       role: "Line, Bar, Column, Pie Charts, Tables ",
+    //       date: "28 Jun - 2 Jul",
+    //     },
+    //     {
+    //       role: "VARC 6",
+    //       date: "2 Jul",
+    //     },
+       
+    //   ],
+    //   open: false,
+    // },
+    // {
+    //   id: 33,
+    //   question: "Week 33 ",
+    //   answer: [
+    //     {
+    //       role: "Linear Arrangement Sectional Test ",
+    //       date: "26 Jun",
+    //     },
+    //     {
+    //       role: "ML Course",
+    //       date: "26 Jun - 2 July",
+    //     },
+    //     {
+    //       role: "Circular Arrangement Sectional Test ",
+    //       date: "27 Jun",
+    //     },
+    //     {
+    //       role: "Line, Bar, Column, Pie Charts, Tables ",
+    //       date: "28 Jun - 2 Jul",
+    //     },
+    //     {
+    //       role: "VARC 6",
+    //       date: "2 Jul",
+    //     },
+       
+    //   ],
+    //   open: false,
+    // },
+    // {
+    //   id: 34,
+    //   question: "Week 34 ",
+    //   answer: [
+    //     {
+    //       role: "Linear Arrangement Sectional Test ",
+    //       date: "26 Jun",
+    //     },
+    //     {
+    //       role: "ML Course",
+    //       date: "26 Jun - 2 July",
+    //     },
+    //     {
+    //       role: "Circular Arrangement Sectional Test ",
+    //       date: "27 Jun",
+    //     },
+    //     {
+    //       role: "Line, Bar, Column, Pie Charts, Tables ",
+    //       date: "28 Jun - 2 Jul",
+    //     },
+    //     {
+    //       role: "VARC 6",
+    //       date: "2 Jul",
+    //     },
+       
+    //   ],
+    //   open: false,
+    // },
     
   ];
   
